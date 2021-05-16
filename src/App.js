1418852344f20c316ffa2c8968bf2384b3b52f0d
rickdouglas <rickmiranda23@gmail.com>
2021-05-16 02:13:09 -0300

    Primeiro commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [busca, setBusca] = useState('');
+  const onInputChange = (e) => {
+    setBusca(e.target.value)
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <section>
+      <form>
+        <label>
+          <span>Google Books API</span>
+          <input
+            type="search"
+            placeholder="Pesquisar por :"
+          />
+          <button type="submit">Search</button>
+        </label>
+      </form>
+    </section>
     </div>
   );
 }
